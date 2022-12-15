e9bc7f4bbf6b188fd9b693753118ee565fe6bfbf
Luana Rodrigues Santos <l.rodrigues.santos@avanade.com>
2022-12-15 08:58:36 -0800

    ajuste tela

--- todo-app/src/screens/fornecedor/autoCadastro/dadosFinanceiros/dadosDRE.js
+++ todo-app/src/screens/fornecedor/autoCadastro/dadosFinanceiros/dadosDRE.js
@@ -348,7 +348,7 @@ const [
 
 				<Card style={{ marginTop: 8 }}>
 					<CardHeader
-						title={translateWithHtml('dadosDemonstracaoResultadoExercicio')}
+						title={translateWithHtml('dadosDemonstracaoResultadoExercicio') + '2022'}
 						action={
 							<Fragment>
 								<Box>
@@ -494,7 +494,7 @@ const [
 
 				<Card style={{ marginTop: 8 }}>
 					<CardHeader
-						title={translateWithHtml('dadosDemonstracaoResultadoExercicio')}
+						title={translateWithHtml('dadosDemonstracaoResultadoExercicio') + '2021'}
 						action={
 							<Fragment>
 								<Box>
@@ -641,7 +641,7 @@ const [
 
 <Card style={{ marginTop: 8 }}>
 					<CardHeader
-						title={translateWithHtml('dadosDemonstracaoResultadoExercicio')}
+						title={translateWithHtml('dadosDemonstracaoResultadoExercicio') + '2020'}
 						action={
 							<Fragment>
 								<Box>
